dfb1cda8834cc435dcca505b2c64f7716f25f658
louismn <louis.martindunord@eemi.com>
2018-09-04 15:53:37 +0200

    modification du fichier exo_13.rb

--- exo_13.rb
+++ exo_13.rb
@@ -5,6 +5,25 @@ def rps (joueur1,joueur2)
 		puts "Le joueur 1 gagne"
 	elsif joueur1 == "rock" && joueur2 == "scissors"
 		puts "Le joueur 1 gagne"
+	elsif joueur1 == "paper" && joueur2 == "paper"
+		puts "égalité"
+	elsif joueur1 == "scissors" && joueur2 == "scissors"
+		puts "égalité"
+	elsif joueur1 == "rock" && joueur2 == "rock"
+		puts "égalité"
+	elsif joueur1 == "paper" && joueur2 == "scissors"
+		puts "Le joueur 2 gagne"
+	elsif joueur1 == "paper" && joueur2 == "rock"
+		puts "Le joueur 2 gagne"
+	elsif joueur1 == "scissors" && joueur2 == "rock"
+		puts "Le joueur 2 gagne"
+	elsif joueur1 == "rock" && joueur2 == "scissors"
+		puts "Le joueur 2 gagne"
+		elsif joueur1 == "rock" && joueur2 == "paper"
+		puts "Le joueur 2 gagne"
+	else
+		puts "il y a une erreur"
+
 	end
 end
 
