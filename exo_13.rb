df1f441d7cb564804bb62a2f7ef6896f5e994232
louismn <louis.martindunord@eemi.com>
2018-09-06 13:30:52 +0200

    amélioration du fichier exo_13.rb

--- exo_13.rb
+++ exo_13.rb
@@ -1,30 +1,28 @@
 def rps (joueur1,joueur2)
-	if joueur1 == "scissors" && joueur2 == "paper"
-		puts "Le joueur 1 gagne"
-	elsif joueur1 == "paper" && joueur2 == "rock"
-		puts "Le joueur 1 gagne"
-	elsif joueur1 == "rock" && joueur2 == "scissors"
-		puts "Le joueur 1 gagne"
-	elsif joueur1 == "paper" && joueur2 == "paper"
-		puts "égalité"
-	elsif joueur1 == "scissors" && joueur2 == "scissors"
-		puts "égalité"
-	elsif joueur1 == "rock" && joueur2 == "rock"
-		puts "égalité"
-	elsif joueur1 == "paper" && joueur2 == "scissors"
-		puts "Le joueur 2 gagne"
-	elsif joueur1 == "paper" && joueur2 == "rock"
-		puts "Le joueur 2 gagne"
-	elsif joueur1 == "scissors" && joueur2 == "rock"
-		puts "Le joueur 2 gagne"
-	elsif joueur1 == "rock" && joueur2 == "scissors"
-		puts "Le joueur 2 gagne"
-		elsif joueur1 == "rock" && joueur2 == "paper"
-		puts "Le joueur 2 gagne"
-	else
-		puts "il y a une erreur"
+	if joueur1 == "paper" && joueur2 == "paper"
+puts "Égalité"
+	elsif joueur1 == "paper" && joueur2 == "scissor" 
+puts "Joueur 2 gagne"
+	elsif joueur1 == "paper" && joueur2 ==	"rock"
+puts "Joueur 1 gagne"
 
-	end
+	elsif joueur1 == "scissor" && joueur2 == "paper"
+puts "Joueur 1 gagne"
+	elsif joueur1 == "scissor" && joueur2 == "scissor" 
+puts "Égalité"
+	elsif joueur1 == "scissor" && joueur2 =="rock"
+puts "Joueur 2 gagne"
+
+elsif joueur1 == "paper" && joueur2 == "paper"
+puts "Égalité"
+	elsif joueur1 == "paper" && joueur2 == "scissor" 
+puts "Joueur 2 gagne"
+	elsif joueur1 == "paper" && joueur2 ==	"rock"
+puts "Joueur 1 gagne"
+else 
+	puts "ceux ci n'est pas une valeur possible"
+end
 end
 
-rps("scissors","paper")
+rps("scissor","paper")
+
